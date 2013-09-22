92d0d5504f6acb9264396fe203d3ec28ef8b32bf
Anthony Borg <teddymcc@Anthonys-MacBook-Air-2.local>
2013-09-22 18:13:35 +1000

    scrolling sorted

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -54,6 +54,45 @@ require(['app', 'jquery', 'bootstrapTransition', 'bootstrapCarousel'], function 
     'use strict';
     // use app here
     //$('.carousel').carousel();
+    $('.carousel').carousel('pause')
     console.log(app);
     console.log('Running jQuery %s', $().jquery);
+
+
+    var scrollStart = 0;
+    var lastScroll = new Date();
+
+            
+            $(document).keydown(function(e){
+                if (e.keyCode == 40) { 
+                    $('.carousel').carousel('next');
+                }
+                if (e.keyCode == 38) { 
+                    $('.carousel').carousel('prev');        
+                }
+            });
+
+            $(window).on('DOMMouseScroll mousewheel', function (e) {
+              
+               //if(e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0) 
+               var currentTime = new Date;
+               if((currentTime.getTime() - lastScroll.getTime()) < 1000)
+               {
+                  return false;
+               }
+
+
+              if(e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0) { 
+                $('.carousel').carousel('next');
+                
+              } else {
+                $('.carousel').carousel('prev');
+             
+              }
+            lastScroll = new Date();
+
+              return false;
+            });
+
+
 });
